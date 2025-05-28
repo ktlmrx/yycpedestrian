efd211e4ff74b8bc3d8614734ab1cb450c4355e4
katie <91563108+ktlmrx@users.noreply.github.com>
2025-05-28 12:24:51 -0600

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const footer = document.createElement("footer");
+  footer.innerHTML = `
+    <p>&copy; 2025 yycpedestrian</p>
+  `;
+  document.body.appendChild(footer);
+});
